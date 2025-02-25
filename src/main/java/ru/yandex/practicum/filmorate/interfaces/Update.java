64467c4603f33b1d6c97b540fc0172d350d5ad10
n5n1m2 <n5n1m2@mail.ru>
2025-02-25 13:09:35 +0300

    create project

--- src/main/java/ru/yandex/practicum/filmorate/interfaces/Update.java
+++
@@ -1,4 +0,0 @@
-package ru.yandex.practicum.filmorate.interfaces;
-
-public interface Update {
-}
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,33 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.*;
-import lombok.Data;
-import lombok.Value;
-import lombok.experimental.NonFinal;
-import ru.yandex.practicum.filmorate.interfaces.Update;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.time.LocalDate;
-
-@Data
-@Value
+/**
+ * Film.
+ */
+@Getter
+@Setter
 public class Film {
-    @NotEmpty
-    String name;
-    @Size(max = 200)
-    String description;
-    @JsonFormat
-    @NotNull
-    LocalDate releaseDate;
-    @NotNull
-    @Min(0)
-    Integer duration;
-    @NonFinal
-    @NotNull(groups = Update.class)
-    Integer id;
-
-    @AssertTrue
-    public boolean isReleaseDateValid() {
-        return releaseDate != null && releaseDate.isAfter(LocalDate.of(1895, 12, 28));
-    }
 }
