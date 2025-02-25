cf7137e0e3e9597ade22cedb8c4e91ad8f9975f4
n5n1m2 <n5n1m2@mail.ru>
2025-02-25 13:09:35 +0300

    create project

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,18 +2,11 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FilmorateApplication {
-    private static ConfigurableApplicationContext context;
-
     public static void main(String[] args) {
-        context = SpringApplication.run(FilmorateApplication.class, args);
-    }
-
-    public static void stopServer() {
-        SpringApplication.exit(context);
+        SpringApplication.run(FilmorateApplication.class, args);
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/IdGenerationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class IdGenerationException extends RuntimeException {
-    public IdGenerationException(String message) {
-        super(message);
-    }
-}
